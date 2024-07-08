060d08e7fa3d4ddd6d4ab21d7d29251a66b38917
Paulo F. Oliveira <paulo.ferraz.oliveira@gmail.com>
2024-07-08 23:45:14 +0100

    Update example: benefit from constraints and adapt to new API

--- example/src/simpler_cowboy_rest_example.erl
+++ example/src/simpler_cowboy_rest_example.erl
@@ -18,14 +18,18 @@ start_cowboy() ->
         {"/health", #{
             methods => [<<"GET">>],
             are_dispatched_to => simpler_cowboy_rest_example_health
+            % , with_constraints => [] % optional
+            % Other state options go here, too
         }},
         {"/kv[/[:k]]", #{
             methods => [<<"PUT">>, <<"GET">>, <<"POST">>, <<"DELETE">>],
-            are_dispatched_to => simpler_cowboy_rest_example_kv
+            are_dispatched_to => simpler_cowboy_rest_example_kv,
+            with_constraints => [{k, restrict_k}]
+            % Other state options go here, too
         }}
     ],
 
-    {ok, _} = simpler_cowboy_rest:start(Routes, TransOpts),
+    {ok, _} = simpler_cowboy_rest:start(clear, Routes, TransOpts, #{}),
     ok.
 
 start_db() ->
